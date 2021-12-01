cde5671c54fdd435222b4901bb71c47c4404b833
Andrew Loree <andy@andyloree.com>
2021-12-01 17:59:45 -0500

    Day 1, functional and procedural

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -8,17 +8,29 @@ fn main() {
         depths.push(line.unwrap().parse::<i32>().unwrap());
     }
     
+    // Procedureal
     // part 1
     println!("Part 1\r\n{}", "-".repeat(10));
-    num_increases(&depths);
+    println!("{}\r\n", num_increases(&depths));
     
     // part 2
     println!("Part 2\r\n{}", "-".repeat(10));
     let summed_depths = summed_sliding_values(&depths, 3);
-    num_increases(&summed_depths);
+    println!("{}\r\n", num_increases(&summed_depths));
+
+    // Functional versions
+    // part 1
+    println!("Functional Part 1\r\n{}", "-".repeat(10));
+    println!("{}\r\n", depths.windows(2).map(|value| { value[1] > value[0] } ).filter(|&increase| increase).count());
+
+    // part 1
+    println!("Functional Part 2\r\n{}", "-".repeat(10));
+    println!("{}", depths.windows(3).map(|sample| { sample.iter().fold(0,|acc,x| { acc + x})}).collect::<Vec<i32>>()
+                         .windows(2).map(|value| { value[1] > value[0] } ).filter(|&increase| increase).count());
+
 }
 
-fn num_increases(depths: &Vec<i32> ) {
+fn num_increases(depths: &Vec<i32> ) -> i32 {
     let mut num_increases: i32 = 0;
     let mut prev_depth: i32 = -1;
     
@@ -29,16 +41,14 @@ fn num_increases(depths: &Vec<i32> ) {
         prev_depth = *cur_depth;
     }
 
-    println!("Number of increases: {}\r\n", num_increases);
+    return num_increases;
 }
 
 fn summed_sliding_values(depths: &Vec<i32>, window: usize) -> Vec<i32> {
     let mut summed_depths:  Vec<i32> = Vec::new();
-    
-    let mut count = 0;
+
     for sample in depths.windows(window) {
         summed_depths.push(sample.iter().sum());
-        count += 1;
     }
 
     return summed_depths;
